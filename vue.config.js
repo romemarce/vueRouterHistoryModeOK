1a2a0ee0351f2dc9e2423d31e375fa4950cc356b
Marcelo <romemarce1@gmail.com>
2021-05-04 15:53:33 -0300

    v1.0

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// vue.config.js 
+module.exports = {
+  // Solo para example.com/subfolder
+  publicPath: process.env.NODE_ENV === 'production' ? '/osfatun-cotizador' : './',
+  
+  // Solo para example.com
+  // publicPath: "./",
+};
